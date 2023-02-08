6a9ce17a8b4edfbed598801a95e9803e45d6f300
mady <madysemega.ms@gmail.com>
2023-02-07 23:39:23 -0500

    Changed socket servers listenning ports on limo and drone

--- Interface/server/app/controllers/client.socket.controller.ts
+++ Interface/server/app/controllers/client.socket.controller.ts
@@ -3,23 +3,39 @@ import { injectable } from 'inversify';
 
 @injectable()
 export class ClientSocketController {
-    private socket: Socket
+    private socket1: Socket;
+    private socket2: Socket;
 
     constructor(){
-        this.socket = io('http://localhost:3000')
+        this.socket1 = io('http://localhost:3020');
+        this.socket2 = io('http://localhost:3030');
     }
 
     connectToServer(){
-        this.socket.on('connect', ()=> {
+        this.socket1.on('connect', ()=> {
 
             //receive a message form the server
-            this.socket.on('server-message', (message) => {
-                console.log('le client a recu un message du serveur')
+            this.socket1.on('server-message', (message) => {
+                console.log('le client Rover a recu un message du serveur: ')
                 console.log(message)
 
                 // send a message back to server
                 setTimeout(() => {
-                    this.socket.emit('client-message', 'Ceci est un message du client')
+                    this.socket1.emit('client-message', 'Ceci est un message du client - Rover')
+                }, 1000);
+            })
+        })
+
+        this.socket2.on('connect', ()=> {
+
+            //receive a message form the server
+            this.socket2.on('server-message', (message) => {
+                console.log('le client Drone a recu un message du serveur: ')
+                console.log(message)
+
+                // send a message back to server
+                setTimeout(() => {
+                    this.socket2.emit('client-message', 'Ceci est un message du client - Drone')
                 }, 1000);
             })
         })
--- Interface/server/app/controllers/server.socket.controller.ts
+++ Interface/server/app/controllers/server.socket.controller.ts
@@ -3,13 +3,16 @@ import { Server as SocketServer } from 'socket.io';
 
 
 export class ServerSocketController {
-    private io: SocketServer
+    private io: SocketServer;
+    private port: number;
 
     constructor(io: SocketServer) {
         this.io = io;
+        this.port = 3010;
     }
 
     init() {
+        this.io.listen(this.port);
         this.io.on('connection', (socket) => {
             console.log('Le server a recu une connection du socket:')
 
