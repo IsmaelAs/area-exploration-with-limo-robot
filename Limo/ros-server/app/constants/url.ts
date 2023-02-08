bbf1eb3e306fba89c64be94ca7d81350ed6db69c
koceilal <koceila.lakhdari@polymtl.ca>
2023-02-08 01:30:56 -0500

    correcte url for bridge and node server

--- Limo/ros-server/app/constants/url.ts
+++ Limo/ros-server/app/constants/url.ts
@@ -1,5 +1,2 @@
-import { env } from "process";
-
-export const ROS_MASTER_URI = `${env.ROS_MASTER_URI}` || "http://localhost:11311"
-
+export const ROS_MASTER_URI = "ws://limo-bridge-server:9090"
 export const URL_NODE_SERVER = 'http://limo-node-server:8080/'
