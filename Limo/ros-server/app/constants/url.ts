4ab70553f94d5d999cced15582dda3a3489b486e
koceilal <koceila.lakhdari@polymtl.ca>
2023-02-08 05:56:38 -0500

    put right port for node server

--- Limo/ros-server/app/constants/url.ts
+++ Limo/ros-server/app/constants/url.ts
@@ -1,2 +1,2 @@
 export const ROS_MASTER_URI = "ws://limo-bridge-server:9090"
-export const URL_NODE_SERVER = 'http://limo-node-server:8080/'
+export const URL_NODE_SERVER = 'http://limo-node-server:9332'
