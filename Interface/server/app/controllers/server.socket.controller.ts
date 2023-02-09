4a48f445a6a92b348bbf678c5c9d44a774abd14e
koceilal <koceila.lakhdari@polymtl.ca>
2023-02-09 03:43:58 -0500

    remove useless comment

--- Interface/server/app/controllers/server.socket.controller.ts
+++ Interface/server/app/controllers/server.socket.controller.ts
@@ -4,29 +4,21 @@ import { Server as SocketServer } from 'socket.io';
 
 export class ServerSocketController {
     private io: SocketServer;
-   // private port: number;
 
     constructor(io: SocketServer) {
         this.io = io;
-      //  this.port = 3010;
     }
 
     init() {
-        //this.io.listen(this.port);
         this.io.on('connection', (socket) => {
             console.log('Le server a recu une connection du socket:')
 
-            //receive a message from client
-            socket.on('client-message', (message) => {
-                console.log('Le serveur a recu un message du client')
-                console.log(message)
-            }) 
 
             socket.on('avancer', () => {
-               console.log("avancer dans socket controller");
-               this.io.emit('limo-avancer');
-           })
-     
+                console.log("avancer dans socket controller");
+                this.io.emit('limo-avancer');
+            })
+
 
         })
     }
