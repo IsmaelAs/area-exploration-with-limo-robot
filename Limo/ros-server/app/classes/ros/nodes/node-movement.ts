432d68840863c9532d568498c24ce18388604767
koceilal <koceila.lakhdari@polymtl.ca>
2023-02-10 14:12:40 -0500

    reduce delay between limo request in node movement

--- Limo/ros-server/app/classes/ros/nodes/node-movement.ts
+++ Limo/ros-server/app/classes/ros/nodes/node-movement.ts
@@ -92,7 +92,7 @@ export class NodeMovement {
         const msg = new Message(data)
         for(let _ = 0; _ < nbrSendingMsg; _++) {
             this.publisherMovement.publish(msg)
-            await delay(550)
+            await delay(525)
         }
         this.publisherMovement.publish(this.nulVelocityMsg)
     }
