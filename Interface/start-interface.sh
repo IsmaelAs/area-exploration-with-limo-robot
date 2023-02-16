e6ba4aa5f9e44bfe8f71fd25c79da648b58ddaf9
koceilal <koceila.lakhdari@polymtl.ca>
2023-02-16 16:37:52 -0500

    change start interface to connect to windows port

--- Interface/start-interface.sh
+++ Interface/start-interface.sh
@@ -2,7 +2,7 @@
 
 LIMO_IP_1=$1
 LIMO_IP_2=$2
-BACKEND_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+BACKEND_IP=$(ipconfig.exe | grep -a 'Wi-Fi' -A 4 | grep -a 'IPv4' | cut -d":" -f 2  | tail -n1 | sed -e 's/\s*//g')
 
 docker-compose -f interface-docker-compose.yml build
 BACKEND_IP=$BACKEND_IP LIMO_IP_1=$LIMO_IP_1 LIMO_IP_2=$LIMO_IP_2 docker-compose -f interface-docker-compose.yml up
