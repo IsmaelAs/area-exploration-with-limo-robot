7cb56d90372234012eda1ac332fe0f1454b8e0df
koceilal <koceila.lakhdari@polymtl.ca>
2023-02-23 10:44:49 -0800

    remove useless comment ; works on ubuntu

--- Limo/ros-packages/entrypoint.sh
+++ Limo/ros-packages/entrypoint.sh
@@ -3,11 +3,5 @@
 source /opt/ros/noetic/setup.bash 
 hostname -I
 
-ROS_IP=$(hostname -I | head -n1 | awk '{print $1;}')
-# echo $ROS_IP
-# export ROS_IP=$ROS_IP
-export ROS_HOSTNAME=$ROS_IP
-# export ROS_HOSTNAME=ubuntu.local
-
 
 roslaunch rosbridge_server rosbridge_websocket.launch 
