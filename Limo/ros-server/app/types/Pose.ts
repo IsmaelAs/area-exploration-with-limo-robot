94a538e27921d79d569c81976f66960099ba6f09
koceilal <koceila.lakhdari@polymtl.ca>
2023-02-23 10:09:35 -0500

    create all types for odometry

---
+++ Limo/ros-server/app/types/Pose.ts
@@ -0,0 +1,9 @@
+import Point from "./Point"
+import Quaternion from "./Quaternion"
+
+type Pose = {
+    position: Point,
+    orientation: Quaternion
+}
+
+export default Pose
---
+++ Limo/ros-server/app/types/Quaternion.ts
@@ -0,0 +1,8 @@
+type Quaternion = {
+    x: number, 
+    y: number, 
+    z: number, 
+    w: number
+}
+
+export default Quaternion
