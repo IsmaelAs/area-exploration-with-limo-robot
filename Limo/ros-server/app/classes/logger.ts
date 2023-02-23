67a7cb8c66bedd73577c554d50be0cc6bf806e2d
koceilal <koceila.lakhdari@polymtl.ca>
2023-02-23 10:28:28 -0500

    log position and distance

--- Limo/ros-server/app/classes/logger.ts
+++ Limo/ros-server/app/classes/logger.ts
@@ -16,9 +16,19 @@ export class Logger {
     }
 
     private callBack() {
-        const data = this.nodePosition.getData()
+        const position = this.positionLog()
         const limoId = process.env.LIMO_ID
-        this.socketServer.emit("save-log", {limoId: limoId, data: data})
+        this.socketServer.emit("save-log", {limoId: limoId, data: position})
+    }
+
+    private positionLog() {
+        const data = this.nodePosition.getData()
+        const distance = Math.sqrt(Math.pow(data.pose.pose.position.x, 2) + Math.pow(data.pose.pose.position.y, 2) + Math.pow(data.pose.pose.position.z, 2))
+        const position = {
+            position: data.pose.pose.position,
+            distance: distance
+        }
+        return position
     }
 
     stopLog() {
