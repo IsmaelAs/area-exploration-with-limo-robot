5a8961a11abc362fa712e6acf27a69675d279132
koceilal <koceila.lakhdari@polymtl.ca>
2023-02-23 10:11:13 -0500

    create node position

---
+++ Limo/ros-server/app/classes/ros/nodes/node-position.ts
@@ -0,0 +1,31 @@
+import { Ros, Topic } from 'roslib'
+import { BRIDGE_URI } from '../../../constants/url'
+import Odometry from '@app/types/Odometry'
+
+export class NodePosition {
+    private ros: Ros
+    private subscriberMovement: Topic
+    private data: Odometry
+
+
+    initNodePosition() {
+        this.ros = new Ros({url: BRIDGE_URI})
+        this.subscriberMovement = new Topic({
+            ros: this.ros, 
+            name: "odom",
+            messageType: "nav_msgs/Odometry",
+            queue_size: 10
+        })
+
+        this.subscriberMovement.subscribe(this.callBack)
+    }
+
+
+    private callBack(data: Odometry) {
+        this.data = data
+    }
+
+    getData() {
+        return this.data
+    }
+}
