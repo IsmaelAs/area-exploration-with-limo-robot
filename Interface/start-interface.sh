44380e2e06642ecbe0100415eea3d86aa31f959c
koceilal <koceila.lakhdari@polymtl.ca>
2023-02-24 15:11:46 -0800

    change way get backend ip for linux

--- Interface/start-interface.sh
+++ Interface/start-interface.sh
@@ -2,7 +2,7 @@
 
 LIMO_IP_1=$1
 LIMO_IP_2=$2
-BACKEND_IP=$(ipconfig.exe | grep -a 'Wi-Fi' -A 4 | grep -a 'IPv4' | cut -d":" -f 2  | tail -n1 | sed -e 's/\s*//g')
+BACKEND_IP=$(hostname -I | head -n1 | awk '{print $1;}')
 
 docker-compose -f interface-docker-compose.yml build
 BACKEND_IP=$BACKEND_IP LIMO_IP_1=$LIMO_IP_1 LIMO_IP_2=$LIMO_IP_2 docker-compose -f interface-docker-compose.yml up
