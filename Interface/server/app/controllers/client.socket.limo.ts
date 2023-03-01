7b39f6e751ea404f7b02051c7e4c0f300c4896be
koceilal <koceila.lakhdari@polymtl.ca>
2023-03-01 03:04:47 -0800

    emit after all subscription in socket

--- Interface/server/app/controllers/client.socket.limo.ts
+++ Interface/server/app/controllers/client.socket.limo.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 import { LIMO_URL } from '../constants/url';
-import { Logger } from '@app/services/logger';
+import { Logger } from '../services/logger';
 import LogLimo from '@app/interfaces/log-limo';
 
 export class ClientSocketLimo {
@@ -11,17 +11,19 @@ export class ClientSocketLimo {
     constructor(limoId: number) {
         this.limoId = limoId
         this.socket = io(LIMO_URL);
+        this.logger = new Logger()
     }
 
     connectClientSocketToLimo() {
         this.socket.on("connect", () => {
             console.log(`Limo ${this.limoId} connected to the ROS server`);
             this.logger.saveLimoData({limoId: this.limoId, data: `Limo ${this.limoId} connected to the ROS server`})
-            this.socket.emit("login", this.limoId)
-
+            
             this.socket.on("save-log", (data: LogLimo) => {
                 this.logger.saveLimoData(data)
             })
+
+            this.socket.emit("login", this.limoId)
         })
     }
 
