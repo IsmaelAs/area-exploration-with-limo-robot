62b58a3adfc81ad3857683c87bbc9a52b206cf27
koceilal <koceila.lakhdari@polymtl.ca>
2023-03-01 03:08:58 -0800

    - create observable to open logs dialogs - subscribe to openLogs subject of  socketCommunication - open dialog when receiving  them

--- Interface/client/src/app/app.component.ts
+++ Interface/client/src/app/app.component.ts
@@ -1,21 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { SocketCommunicationService } from './services/socket-communication/socket-communication.service';
 import RobotTargetType from './types/RobotType';
-
+import { MatDialog, MatDialogRef } from "@angular/material/dialog"
+import { LogsDialogComponent } from './dialogs/logs-dialog/logs-dialog.component';
+import {  Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnDestroy {
 
   list: RobotTargetType[] = ["limo-1", "limo-2", "robots"];
   type: RobotTargetType = "limo-1";
+  missionNumber: number = 0;
+  private openLogsSubscription: Subscription;
+  private ref: MatDialogRef<LogsDialogComponent> | undefined;
   
   constructor(
-    private socketCommunication : SocketCommunicationService
-    ){}
+    private socketCommunication : SocketCommunicationService,
+    private matDialogLogsOpen: MatDialog,
+    ){
+      this.openLogsSubscription = socketCommunication.subscribeOpenLogs.subscribe(this.openLogsDialog.bind(this))
+    }
   
+  
+  ngOnDestroy () {
+    this.openLogsSubscription.unsubscribe()
+  }
 
   identify(){    
     this.socketCommunication.identify(this.type);
@@ -32,6 +44,20 @@ export class AppComponent {
 
   setType(choice: RobotTargetType) { 
     this.type = choice;
+  }
+
+
+  showLog() {
+    this.socketCommunication.showLog(this.missionNumber)
+  }
+
+  private openLogsDialog(logs: string) {
+    if (this.ref) this.ref.close()
+    this.ref = this.matDialogLogsOpen.open(LogsDialogComponent, {
+      data: {
+        logs
+      }
+    })
   }
 
 }
