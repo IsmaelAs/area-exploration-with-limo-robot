13df1334c1676a55f83605eb71e1ffbb5415102e
koceilal <koceila.lakhdari@polymtl.ca>
2023-03-02 04:59:43 -0800

    correct file with eslint for interface backend

--- Interface/server/app/server.ts
+++ Interface/server/app/server.ts
@@ -1,3 +1,6 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-undef */
+/* eslint-disable no-magic-numbers */
 import * as http from 'http';
 import { AddressInfo } from 'net';
 import { Service } from 'typedi';
@@ -10,86 +13,91 @@ import { ClientSocketLimo2 } from './controllers/client.socket.limo2';
 
 @Service()
 export class Server {
-    private static readonly appPort: string | number | boolean = Server.normalizePort(9330);
-    private static readonly baseDix: number = 10;
-    private server: http.Server;
-    private io: SocketServer;
-    private serverSocketController: ServerSocketController;
-    private socketLimo?: ClientSocketLimo1
-    private socketLimo2?: ClientSocketLimo2
+  private static readonly appPort: string | number | boolean = Server.normalizePort(9330);
+
+  private static readonly baseDix: number = 10;
+
+  private server: http.Server;
+
+  private io: SocketServer;
+
+  private serverSocketController: ServerSocketController;
+
+  private socketLimo?: ClientSocketLimo1;
+
+  private socketLimo2?: ClientSocketLimo2;
 
 
-    constructor(private readonly application: Application) {}
+  // eslint-disable-next-line no-useless-constructor, no-empty-function
+  constructor(private readonly application: Application) {}
 
-    private static normalizePort(val: number | string): number | string | boolean {
-        const port: number = typeof val === 'string' ? parseInt(val, this.baseDix) : val;
-        if (isNaN(port)) {
-            return val;
-        } else if (port >= 0) {
-            return port;
-        } else { 
-            return false;
-        }
+  private static normalizePort(val: number | string): number | string | boolean {
+    const port: number = typeof val === 'string' ? parseInt(val, this.baseDix) : val;
+    if (isNaN(port)) {
+      return val;
+    } else if (port >= 0) {
+      return port;
+    }
+    return false;
+  }
+
+  init(): void {
+    this.application.app.set('port', Server.appPort);
+    this.server = http.createServer(this.application.app);
+    console.log(Server.appPort);
+
+    this.server.listen(Server.appPort);
+    this.server.on('error', (error: NodeJS.ErrnoException) => this.onError(error));
+    this.server.on('listening', () => this.onListening());
+
+    console.log('Init socket manager');
+    this.io = new SocketServer(this.server, {
+      cors: {
+        origin: '*',
+      },
+    });
+
+    if (process.env.LIMO_IP_1) {
+      this.socketLimo = new ClientSocketLimo1();
+      this.socketLimo.connectClientSocketToLimo1();
+    }
+    if (process.env.LIMO_IP_2) {
+      this.socketLimo2 = new ClientSocketLimo2();
+      this.socketLimo2.connectClientSocketToLimo2();
     }
 
-    init(): void {
-        this.application.app.set('port', Server.appPort);
-        this.server = http.createServer(this.application.app);
-        console.log(Server.appPort);
-        
-        this.server.listen(Server.appPort);
-        this.server.on('error', (error: NodeJS.ErrnoException) => this.onError(error));
-        this.server.on('listening', () => this.onListening());
-        
-        console.log("Init socket manager");
-        this.io = new SocketServer(this.server, {
-            cors: {
-                origin: "*"
-            }
-        })
+    this.serverSocketController = new ServerSocketController(this.io, this.socketLimo, this.socketLimo2);
+    this.serverSocketController.initializeSocketServer();
+  }
 
-        if (process.env.LIMO_IP_1) {
-            this.socketLimo = new ClientSocketLimo1()
-            this.socketLimo.connectClientSocketToLimo1()
-        }
-        if (process.env.LIMO_IP_2) {
-            this.socketLimo2 = new ClientSocketLimo2()
-            this.socketLimo2.connectClientSocketToLimo2()
-        }
-
-        this.serverSocketController = new ServerSocketController(this.io, this.socketLimo, this.socketLimo2);
-        this.serverSocketController.initializeSocketServer();
-
+  private onError(error: NodeJS.ErrnoException): void {
+    if (error.syscall !== 'listen') {
+      throw error;
     }
-
-    private onError(error: NodeJS.ErrnoException): void {
-        if (error.syscall !== 'listen') {
-            throw error;
-        }
-        const bind: string = typeof Server.appPort === 'string' ? 'Pipe ' + Server.appPort : 'Port ' + Server.appPort;
-        switch (error.code) {
-            case 'EACCES':
-                // eslint-disable-next-line no-console
-                console.error(`${bind} requires elevated privileges`);
-                process.exit(1);
-                break;
-            case 'EADDRINUSE':
-                // eslint-disable-next-line no-console
-                console.error(`${bind} is already in use`);
-                process.exit(1);
-                break;
-            default:
-                throw error;
-        }
-    }
-
-    /**
-     * Se produit lorsque le serveur se met à écouter sur le port.
-     */
-    private onListening(): void {
-        const addr = this.server.address() as AddressInfo;
-        const bind: string = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
+    const bind: string = typeof Server.appPort === 'string' ? `Pipe ${Server.appPort}` : `Port ${Server.appPort}`;
+    switch (error.code) {
+      case 'EACCES':
         // eslint-disable-next-line no-console
-        console.log(`Listening on ${bind}`);
+        console.error(`${bind} requires elevated privileges`);
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        // eslint-disable-next-line no-console
+        console.error(`${bind} is already in use`);
+        process.exit(1);
+        break;
+      default:
+        throw error;
     }
+  }
+
+  /**
+   * Se produit lorsque le serveur se met à écouter sur le port.
+   */
+  private onListening(): void {
+    const addr = this.server.address() as AddressInfo;
+    const bind: string = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
+    // eslint-disable-next-line no-console
+    console.log(`Listening on ${bind}`);
+  }
 }
