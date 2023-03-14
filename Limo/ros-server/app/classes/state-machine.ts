1a022e1df8deba6ebe0bba46c7667d7d98c22cb4
youssef137ch <youssef.chourifi@polymtl.ca>
2023-03-14 01:03:53 -0700

    state machine uncomented in Limo server

--- Limo/ros-server/app/classes/state-machine.ts
+++ Limo/ros-server/app/classes/state-machine.ts
@@ -1,64 +1,81 @@
-// import { BRIDGE_URI } from '@app/constants/url';
-// import { Ros, Topic } from 'roslib';
-// import { State } from '@app/types/States';
-// import { SocketServer } from './socket-server';
-// import { setInterval } from 'timers';
+
+import { BRIDGE_URI } from '@app/constants/url';
+import { Ros, Topic } from 'roslib';
+import { State } from '@app/types/States';
+import { SocketServer } from './socket-server';
+import { setInterval } from 'timers';
+
 
 export class MyStateMachine {
-  // private currentState: State = "INIT";
-  // private ros: Ros;
-  // private socketServer: SocketServer;
-  
-  // batteryPercentage: any;
-  // onBatteryMessage: any;
-  // intervalState: any;
 
-  // constructor(socketServer: SocketServer) {
-  //   this.socketServer = socketServer
-  //   // Connexion au serveur ROS
-  //   this.ros = new Ros({
-  //     url: BRIDGE_URI,
-  //   });
+ 
+ private currentState: State = "INIT";
+ private ros: Ros;
+ private socketServer: SocketServer;
+ 
 
-  //     // Abonnement au topic "battery_state"
-  //     const batterySubscriber = new Topic({
-  //       ros: this.ros,
-  //       name: '/battery_state',
-  //       messageType: 'sensor_msgs/BatteryState',
-  //     });
-  //   batterySubscriber.subscribe(this.callBack.bind(this));
-  //   }
-
-  // private callBack(data: {percentage: number}) {
-  //     this.onLowBattery(data.percentage)
-  //     const limoId = this.socketServer.limoId
-  //     this.socketServer.emit("save-state", {limoId: limoId, state: this.currentState})
-  // }
-
-  // startStates() {
-  //   this.intervalState = setInterval(this.callBack, 1000)
-  // }
+ BatteryPercentage: any;
+ onBatteryMessage: any;
+ intervalState: any;
 
 
-  // onLowBattery(percentage: number): void {
 
-  //   // Check if the battery is below 30%
-  //   if (percentage < 30) {
-  //     console.log('Battery level is below 30%');
-  //     this.currentState = "STOPPED";
-  //   } else if (this.currentState !== "ON_MISSION"  && this.currentState !== "WAITING") {
-  //       this.currentState = "WAITING";
-  //   } 
+ Constructor(socketServer: SocketServer) {
+   this.socketServer = socketServer
+   // Connexion au serveur ROS
+   this.ros = new Ros({
+     url: BRIDGE_URI,
+   });
+ 
 
-  // }
-      
-  // onMission() {
-  //   this.currentState = "ON_MISSION";
-  // }
-  // onMissionEnd() {
-  //   this.currentState = "STOPPED";
-  // }
+
+     // Abonnement au topic "battery_state"
+     const batterySubscriber = new Topic({
+       ros: this.ros,
+       name: '/battery_state',
+       messageType: 'sensor_msgs/BatteryState',
+     });
+   batterySubscriber.subscribe(this.callBack.bind(this));
+  }
+
+
+
+ private callBack(data: {percentage: number}) {
+     this.onLowBattery(data.percentage)
+     const limoId = this.socketServer.limoId
+     this.socketServer.emit("save-state", {limoId: limoId, state: this.currentState})
+ }
+ 
+
+
+ StartStates() {
+   this.intervalState = setInterval(this.callBack, 1000)
+ }
+ 
+
+
+ onLowBattery(percentage: number): void {
+
+
+   // Check if the battery is below 30%
+   if (percentage < 30) {
+     console.log('Battery level is below 30%');
+     this.currentState = "STOPPED";
+   } else if (this.currentState !== "ON_MISSION"  && this.currentState !== "WAITING") {
+       this.currentState = "WAITING";
+   }
+ 
+
+ }
+
+
+ OnMission() {
+  this.currentState = "ON_MISSION";
+ }
+
+ onMissionEnd() {
+  this.currentState = "STOPPED";
+ }
 }
-
 
 
