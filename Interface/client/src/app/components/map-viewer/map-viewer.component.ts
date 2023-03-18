198788519b0a7ebfddd330c7c78f7eeced3d7b39
hannan <hannan.dissou@polymtl.ca>
2023-03-18 17:22:24 -0400

    added map for limo 2 and potential merged map

--- Interface/client/src/app/components/map-viewer/map-viewer.component.ts
+++ Interface/client/src/app/components/map-viewer/map-viewer.component.ts
@@ -11,9 +11,12 @@ import { GridClient, MapViewer } from 'src/app/types/ros3d';
 export class MapViewerComponent {
 
   private limo_bridge: string = "ws://localhost:9090"
-  private gridClient: GridClient = { }
-  private viewer_1: MapViewer = { scene: undefined}
-  private viewer_2: MapViewer = { scene: undefined}
+  private gridClient1: GridClient = { }
+  private gridClient2: GridClient = { }
+  private gridClient3: GridClient = { }
+  private viewer1: MapViewer = { scene: undefined}
+  private viewer2: MapViewer = { scene: undefined}
+  private viewer3: MapViewer = { scene: undefined}
   private ros: Ros
 
   constructor() {
@@ -25,17 +28,54 @@ export class MapViewerComponent {
   init() {
 
         // Create the main viewer 1.
-        this.viewer_1 = new ROS3D.Viewer({
-          divID : 'map',
+        this.viewer1 = new ROS3D.Viewer({
+          background: "#7e7e7e",
+          divID : 'map1',
           width : 800,
           height : 600,
           antialias : true
         });
     
-        // Setup the map client.
-        this.gridClient = new ROS3D.OccupancyGridClient({
+        // Setup the map client 1.
+        this.gridClient1 = new ROS3D.OccupancyGridClient({
           ros : this.ros,
-          rootObject : this.viewer_1.scene,
+          topic: "/limo1/map",
+          rootObject : this.viewer1.scene,
+          continuous: true
+        });
+
+        // Create the main viewer 2.
+        this.viewer2 = new ROS3D.Viewer({
+          background: "#7e7e7e",
+          divID : 'map2',
+          width : 800,
+          height : 600,
+          antialias : true
+        });
+    
+        // Setup the map client 2.
+        this.gridClient2 = new ROS3D.OccupancyGridClient({
+          ros : this.ros,
+          topic: "/limo2/map",
+          rootObject : this.viewer2.scene,
+          continuous: true
+        });
+
+
+        // Create the Merged Map viewer 2.
+        this.viewer3 = new ROS3D.Viewer({
+          background: "#7e7e7e",
+          divID : 'map3',
+          width : 800,
+          height : 600,
+          antialias : true
+        });
+    
+        // Setup the Merged map client.
+        this.gridClient3 = new ROS3D.OccupancyGridClient({
+          ros : this.ros,
+          topic: "/map_merge/x_merged_map",
+          rootObject : this.viewer3.scene,
           continuous: true
         });
   }
