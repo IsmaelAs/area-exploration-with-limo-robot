0c308c057932d2a33a2034e5aa15e9d85339ce02
hannan <hannan.dissou@polymtl.ca>
2023-03-19 17:19:56 -0400

    created an empty_pkg to find

---
+++ Limo/ros-packages/packages/build/empty_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/Documents/INF3995-102/Limo/ros-packages/packages/src/empty_pkg
+# Build directory: /home/ubuntu/Documents/INF3995-102/Limo/ros-packages/packages/build/empty_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
