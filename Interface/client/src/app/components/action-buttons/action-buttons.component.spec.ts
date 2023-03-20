051132b3a5f79f6bd5a15ac987d5f608376136cf
koceilal <koceila.lakhdari@polymtl.ca>
2023-03-19 17:33:50 -0700

    action buttons tests works

--- Interface/client/src/app/components/action-buttons/action-buttons.component.spec.ts
+++ Interface/client/src/app/components/action-buttons/action-buttons.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ActionButtonsComponent } from './action-buttons.component';
+import { SocketCommunicationService } from 'src/app/services/socket-communication/socket-communication.service';
+
 
 describe('ActionButtonsComponent', () => {
   let component: ActionButtonsComponent;
   let fixture: ComponentFixture<ActionButtonsComponent>;
+  let socketCommunicationSpy: jasmine.SpyObj<SocketCommunicationService>
 
   beforeEach(async () => {
+  
+    socketCommunicationSpy = jasmine.createSpyObj("SocketCommunicationService", ["identify", "startMission", "stopMission"])
     await TestBed.configureTestingModule({
-      declarations: [ ActionButtonsComponent ]
+      declarations: [ ActionButtonsComponent ],
+      providers: [
+        {provide: SocketCommunicationService, useValue: socketCommunicationSpy}
+      ]
     })
     .compileComponents();
 
