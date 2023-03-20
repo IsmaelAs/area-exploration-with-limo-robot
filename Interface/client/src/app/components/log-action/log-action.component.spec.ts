a8e2aa8c322bb839312b61690eb0b5cb8d3b8ed6
koceilal <koceila.lakhdari@polymtl.ca>
2023-03-19 17:52:14 -0700

    log action tests works

--- Interface/client/src/app/components/log-action/log-action.component.spec.ts
+++ Interface/client/src/app/components/log-action/log-action.component.spec.ts
@@ -1,14 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { LogActionComponent } from './log-action.component';
+import { SocketCommunicationService } from 'src/app/services/socket-communication/socket-communication.service';
+import { MatDialog } from '@angular/material/dialog';
+import { Subject } from 'rxjs';
 
 describe('LogActionComponent', () => {
   let component: LogActionComponent;
   let fixture: ComponentFixture<LogActionComponent>;
+  let socketCommunicationServiceSpy: jasmine.SpyObj<SocketCommunicationService>
+  let matDialogSpy: jasmine.SpyObj<MatDialog>
+  let logsObservable: Subject<string> = new Subject()
 
   beforeEach(async () => {
+    socketCommunicationServiceSpy = jasmine.createSpyObj("SocketCommunicationService", ["getSubscribeOpenLogs"])
+    matDialogSpy = jasmine.createSpyObj("MatDialog", ["open"])
+
+    socketCommunicationServiceSpy.getSubscribeOpenLogs.and.returnValue(logsObservable.asObservable())
+
+
     await TestBed.configureTestingModule({
-      declarations: [ LogActionComponent ]
+      imports: [FormsModule],
+      declarations: [ LogActionComponent ],
+      providers: [
+        {provide: SocketCommunicationService, useValue: socketCommunicationServiceSpy},
+        {provide: MatDialog, useValue: matDialogSpy}
+      ]
     })
     .compileComponents();
 
