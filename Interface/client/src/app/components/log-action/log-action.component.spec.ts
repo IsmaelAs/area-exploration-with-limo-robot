93387510b936f2d686fed0c615eda37319ce6e8c
koceilal <koceila.lakhdari@polymtl.ca>
2023-03-19 21:27:49 -0700

    log action component unittest done

--- Interface/client/src/app/components/log-action/log-action.component.spec.ts
+++ Interface/client/src/app/components/log-action/log-action.component.spec.ts
@@ -2,8 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { LogActionComponent } from './log-action.component';
 import { SocketCommunicationService } from 'src/app/services/socket-communication/socket-communication.service';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Subject } from 'rxjs';
+import { LogsDialogComponent } from 'src/app/dialogs/logs-dialog/logs-dialog.component';
 
 describe('LogActionComponent', () => {
   let component: LogActionComponent;
@@ -13,7 +14,7 @@ describe('LogActionComponent', () => {
   let logsObservable: Subject<string> = new Subject()
 
   beforeEach(async () => {
-    socketCommunicationServiceSpy = jasmine.createSpyObj("SocketCommunicationService", ["getSubscribeOpenLogs"])
+    socketCommunicationServiceSpy = jasmine.createSpyObj("SocketCommunicationService", ["getSubscribeOpenLogs", "showLog"])
     matDialogSpy = jasmine.createSpyObj("MatDialog", ["open"])
 
     socketCommunicationServiceSpy.getSubscribeOpenLogs.and.returnValue(logsObservable.asObservable())
@@ -37,4 +38,43 @@ describe('LogActionComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should ca;; unsubscribe when we call ngOnDestroy", () => {
+    const spyUnsubscribe = spyOn(component["openLogsSubscription"], "unsubscribe").and.callFake(() => {})
+
+    component.ngOnDestroy()
+
+    expect(spyUnsubscribe).toHaveBeenCalled()
+  })
+
+  it("should have call getSubscribeOpenLogs when the component is created", () => {
+    expect(socketCommunicationServiceSpy.getSubscribeOpenLogs).toHaveBeenCalled()
+  })
+
+  it("should call showLog of communication socket when we call showLog", () => {
+    component.showLog()
+
+    expect(socketCommunicationServiceSpy.showLog).toHaveBeenCalled()
+  })
+
+  it("should only call open when the ref is undefined", () => {
+  
+    component["ref"] = undefined
+    component["openLogsDialog"]("test")
+    expect(matDialogSpy.open).toHaveBeenCalled()
+  })
+
+  it("should call close when ref is defined", () => {
+    component["ref"] = {
+      close: () => {}
+    } as unknown as MatDialogRef<LogsDialogComponent, any>
+
+      const spyClose = spyOn(component["ref"], "close")
+
+          component["openLogsDialog"]("test")
+      expect(matDialogSpy.open).toHaveBeenCalled()
+      expect(spyClose).toHaveBeenCalled()
+
+
+  })
 });
