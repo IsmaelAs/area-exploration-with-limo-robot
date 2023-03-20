cccb6ff5a17b76cbce6f5c7ed218cbfefa3b863f
mady <mady@winos>
2023-03-19 20:47:08 -0400

    Added explore_control package

--- Limo/ros-packages/entrypoint.sh
+++ Limo/ros-packages/entrypoint.sh
@@ -1,5 +1,25 @@
-#! /bin/bash
+#!/bin/bash
 
-source /opt/ros/noetic/setup.bash 
+source /opt/ros/noetic/setup.bash
 echo $(hostname -I)
-exec roslaunch rosbridge_server rosbridge_websocket.launch 
+
+# Launch gmapping
+xterm -e "roslaunch limo_navigation gmapping.launch" &
+
+# Wait for gmapping to start up
+sleep 5
+
+# Launch AMCL
+xterm -e "roslaunch limo_navigation amcl.launch" &
+
+# Wait for AMCL to start up
+sleep 5
+
+# Launch move_base
+xterm -e "roslaunch limo_navigation move_base.launch" &
+
+# Wait for move_base to start up
+sleep 5
+
+# Launch explore_lite
+xterm -e "roslaunch limo_navigation explore_costmap.launch" &
