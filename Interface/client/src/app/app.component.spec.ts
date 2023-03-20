3b0c4db69e81a454cbbbd96bbdcdd8589ab828e2
koceilal <koceila.lakhdari@polymtl.ca>
2023-03-19 23:40:43 -0700

    app component unittest done

--- Interface/client/src/app/app.component.spec.ts
+++ Interface/client/src/app/app.component.spec.ts
@@ -1,19 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { IpLimoHandlerComponent } from './components/ip-limo-handler/ip-limo-handler.component';
+import { LogActionComponent } from './components/log-action/log-action.component';
+import { RobotsActionsComponent } from './components/robots-actions/robots-actions.component';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
+  let component: AppComponent
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FormsModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        IpLimoHandlerComponent,
+        LogActionComponent,
+        RobotsActionsComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
   });
 
   it('should create the app', () => {
@@ -21,5 +36,13 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it("should isIpSet with the event", () => {
+    component.isIpSet = false
+
+    component.setIsIpSet({ isIpSet: true })
+
+    expect(component.isIpSet).toBeTruthy()
+  })
 
 });
