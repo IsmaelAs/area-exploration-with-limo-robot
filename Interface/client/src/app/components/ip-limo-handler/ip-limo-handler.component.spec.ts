616e7f482997e45f889ddaa034bfa9ec896157d8
koceilal <koceila.lakhdari@polymtl.ca>
2023-03-19 17:29:40 -0700

    -test env works - ip limo handler works

--- Interface/client/src/app/components/ip-limo-handler/ip-limo-handler.component.spec.ts
+++ Interface/client/src/app/components/ip-limo-handler/ip-limo-handler.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { IpLimoHandlerComponent } from './ip-limo-handler.component';
+import { IpHandlerService } from 'src/app/services/ip-handler.service';
+import { FormsModule } from '@angular/forms';
 
 describe('IpLimoHandlerComponent', () => {
   let component: IpLimoHandlerComponent;
   let fixture: ComponentFixture<IpLimoHandlerComponent>;
+  let ipHandlerServiceSpy: jasmine.SpyObj<IpHandlerService>;
+
 
   beforeEach(async () => {
+
+    ipHandlerServiceSpy = jasmine.createSpyObj("IpHandlerService", ["setIps"])
+
     await TestBed.configureTestingModule({
-      declarations: [ IpLimoHandlerComponent ]
+      imports: [ FormsModule ],
+      declarations: [ IpLimoHandlerComponent ],
+      providers: [
+        {provide: IpHandlerService, useValue: ipHandlerServiceSpy}
+      ]
     })
     .compileComponents();
 
