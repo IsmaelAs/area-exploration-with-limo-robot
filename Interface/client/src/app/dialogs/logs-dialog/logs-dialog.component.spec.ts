3e412c0fe1bb4466904d097e186c390322da229b
koceilal <koceila.lakhdari@polymtl.ca>
2023-03-19 18:00:58 -0700

    logs dialog tests works

--- Interface/client/src/app/dialogs/logs-dialog/logs-dialog.component.spec.ts
+++ Interface/client/src/app/dialogs/logs-dialog/logs-dialog.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { LogsDialogComponent } from './logs-dialog.component';
 
 describe('LogsDialogComponent', () => {
   let component: LogsDialogComponent;
   let fixture: ComponentFixture<LogsDialogComponent>;
+  let dialogsRefSpy: jasmine.SpyObj<MatDialogRef<LogsDialogComponent>>
 
   beforeEach(async () => {
+    dialogsRefSpy = jasmine.createSpyObj(MatDialogRef, ["updateSize", "close"])
+
     await TestBed.configureTestingModule({
-      declarations: [ LogsDialogComponent ]
+      imports: [MatDialogModule],
+      declarations: [ LogsDialogComponent ],
+      providers: [
+        {provide: MatDialogRef, useValue: dialogsRefSpy},
+        {provide: MAT_DIALOG_DATA, useValue: {}}
+      ]
     })
     .compileComponents();
 
