a6eaecf0c2123af79d04b21843b93860f9e6f3d4
koceilal <koceila.lakhdari@polymtl.ca>
2023-03-19 21:36:58 -0700

    log dialog unittest done

--- Interface/client/src/app/dialogs/logs-dialog/logs-dialog.component.spec.ts
+++ Interface/client/src/app/dialogs/logs-dialog/logs-dialog.component.spec.ts
@@ -6,16 +6,17 @@ describe('LogsDialogComponent', () => {
   let component: LogsDialogComponent;
   let fixture: ComponentFixture<LogsDialogComponent>;
   let dialogsRefSpy: jasmine.SpyObj<MatDialogRef<LogsDialogComponent>>
-
+  let dataSpy: jasmine.SpyObj<{logs: string}>
   beforeEach(async () => {
     dialogsRefSpy = jasmine.createSpyObj(MatDialogRef, ["updateSize", "close"])
+    dataSpy = {logs: "test"}
 
     await TestBed.configureTestingModule({
       imports: [MatDialogModule],
       declarations: [ LogsDialogComponent ],
       providers: [
         {provide: MatDialogRef, useValue: dialogsRefSpy},
-        {provide: MAT_DIALOG_DATA, useValue: {}}
+        {provide: MAT_DIALOG_DATA, useValue: dataSpy}
       ]
     })
     .compileComponents();
@@ -28,4 +29,22 @@ describe('LogsDialogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should call updateSize when we call ngOnInit", () => {
+    component.ngOnInit()
+
+    expect(dialogsRefSpy.updateSize).toHaveBeenCalled()
+  })
+
+  it("should call close when we call close", () => {
+    component.close()
+
+    expect(dialogsRefSpy.close).toHaveBeenCalled()
+  })
+
+  it("should return logs when we get logsFormatted", () => {
+    dataSpy.logs = "test_v2"
+
+    expect(component.logsFormatted).toBe("test_v2")
+  })
 });
