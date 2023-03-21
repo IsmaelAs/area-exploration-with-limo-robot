9f2b8f96e6df853db9ec80c04b6fc88a4cdae1bc
mady <mady@winos>
2023-03-20 21:44:23 -0400

    Fixed Dockerfile and entrypoint

--- Limo/ros-packages/entrypoint.sh
+++ Limo/ros-packages/entrypoint.sh
@@ -2,24 +2,34 @@
 
 source /opt/ros/noetic/setup.bash
 echo $(hostname -I)
+xterm -e "roslaunch rosbridge_server rosbridge_websocket.launch" &
+
+# chmod explore_control files
+exec chmod +x /catkin_ws/src/explore_control/publish_explore.py
+exec chmod +x /catkin_ws/src/explore_control/control_explore.py
 
 # Launch gmapping
-xterm -e "roslaunch limo_navigation gmapping.launch" &
+xterm -e "rosrun gmapping slam_gmapping scan:=/limo/scan 2> >(grep -v TF_REPEATED_DATA buffer_core)" &
 
 # Wait for gmapping to start up
 sleep 5
 
-# Launch AMCL
-xterm -e "roslaunch limo_navigation amcl.launch" &
+# Launch navigation stack
+xterm -e "roslaunch limo_bringup limo_navigation_diff.launch 2> >(grep -v TF_REPEATED_DATA buffer_core)" &
 
-# Wait for AMCL to start up
+# Wait for navigation stack to start up
 sleep 5
 
-# Launch move_base
-xterm -e "roslaunch limo_navigation move_base.launch" &
 
-# Wait for move_base to start up
+# Launch explore lite
+xterm -e "roslaunch explore_lite explore_costmap.launch 2> >(grep -v TF_REPEATED_DATA buffer_core)" &
+
+# Wait for explore lite to start up
 sleep 5
 
-# Launch explore_lite
-xterm -e "roslaunch limo_navigation explore_costmap.launch" &
+
+# Publish /exploration_state topic 
+rosrun explore_control publish_explore.py &
+
+# Subscribe to /exploration_state topic to control exploration state
+rosrun explore_control control_explore.py &
