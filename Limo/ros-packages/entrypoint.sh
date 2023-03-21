b40f725a9afbad76fa5a471f5492c013ec1b00a3
IsmaelAs <adamsoulerezaould@gmail.com>
2023-03-20 23:08:43 -0400

    Modif Dockerfile

--- Limo/ros-packages/entrypoint.sh
+++ Limo/ros-packages/entrypoint.sh
@@ -2,34 +2,30 @@
 
 source /opt/ros/noetic/setup.bash
 echo $(hostname -I)
-xterm -e "roslaunch rosbridge_server rosbridge_websocket.launch" &
-
-# chmod explore_control files
-exec chmod +x /home/ros-package/agx_ws/src/explore_control/explore_control/src/publish_explore.py
-exec chmod +x /home/ros-package/agx_ws/src/explore_control/explore_control/src/control_explore.py
+roslaunch rosbridge_server rosbridge_websocket.launch &
 
 # Launch gmapping
-xterm -e "rosrun gmapping slam_gmapping scan:=/limo/scan 2> >(grep -v TF_REPEATED_DATA buffer_core)" &
+rosrun gmapping slam_gmapping scan:=/limo/scan 2> >(grep -v TF_REPEATED_DATA buffer_core) &
 
 # Wait for gmapping to start up
 sleep 5
 
 # Launch navigation stack
-xterm -e "roslaunch limo_bringup limo_navigation_diff.launch 2> >(grep -v TF_REPEATED_DATA buffer_core)" &
+roslaunch limo_bringup limo_navigation_diff.launch 2> >(grep -v TF_REPEATED_DATA buffer_core) &
 
 # Wait for navigation stack to start up
 sleep 5
 
 
 # Launch explore lite
-xterm -e "roslaunch explore_lite explore_costmap.launch 2> >(grep -v TF_REPEATED_DATA buffer_core)" &
+roslaunch explore_lite explore_costmap.launch 2> >(grep -v TF_REPEATED_DATA buffer_core) &
 
 # Wait for explore lite to start up
 sleep 5
 
 
 # Publish /exploration_state topic 
-xterm -e "rosrun explore_control publish_explore.py" &
+rosrun explore_control publish_explore.py &
 
 # Subscribe to /exploration_state topic to control exploration state
-xterm -e "rosrun explore_control control_explore.py" &
+exec rosrun explore_control control_explore.py
