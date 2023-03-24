ffee28b25e67bbbfecb43365b883280321b16b71
koceilal <koceila.lakhdari@polymtl.ca>
2023-03-24 05:55:12 -0700

    send goal after starting limo

--- Limo/ros-packages/packages/src/explore_control/src/control_explore.py
+++ Limo/ros-packages/packages/src/explore_control/src/control_explore.py
@@ -1,8 +1,9 @@
 import rospy
 from std_msgs.msg import Bool
 from actionlib_msgs.msg import GoalID
+from move_base_msgs.msg import MoveBaseActionGoal
 import os
-
+import random
 
 class ExplorationControl:
     def __init__(self):
@@ -14,37 +15,40 @@ class ExplorationControl:
             rospy.init_node('exploration_control' + limoId)
             self.subscriberState = rospy.Subscriber(f"limo{limoId}/exploration_state", Bool, self.setExplorationState)
             self.noMove = rospy.Publisher(f"limo{limoId}/move_base/cancel", GoalID, queue_size=10)
+            self.sendGoal = rospy.Publisher(f"limo{limoId}/move_base/goal", MoveBaseActionGoal, queue_size=10)
         else :
             rospy.init_node('exploration_control')
             self.subscriberState = rospy.Subscriber('/exploration_state', Bool, self.setExplorationState)
             self.noMove = rospy.Publisher('/move_base/cancel', GoalID, queue_size=10)
+            self.sendGoal = rospy.Publisher("/move_base/goal", MoveBaseActionGoal, queue_size=10)
+
 
 
         self.isExploring = False
         self.rate = rospy.Rate(1)
-
-        rospy.loginfo('Tout est crée')
         self.stopRobot()
 
     def setExplorationState(self, msg: Bool):
-        rospy.loginfo("recu message pour set ; dans set stat")
         if msg.data != self.isExploring:
 
-            rospy.loginfo(f"set l'exploration a : {msg.data}")
             self.isExploring = msg.data
             
             if(not self.isExploring):
-                rospy.loginfo("commencement de l'arrêt des goals")
                 self.stopRobot()
 
     def stopRobot(self):
-        rospy.loginfo("dans stop robot")
 
         while not self.isExploring :
             msg = GoalID()
             self.noMove.publish(msg)
-            rospy.loginfo("envoie du message d'arret des goal")
             self.rate.sleep()
+        
+        goal = MoveBaseActionGoal()
+        goal.goal.target_pose.pose.position.x = random.randint(0, 3)
+        goal.goal.target_pose.pose.position.y = random.randint(0, 3)
+        goal.goal.target_pose.pose.orientation.w = 0.66
+
+        self.sendGoal.publish(goal)
 
     
 
