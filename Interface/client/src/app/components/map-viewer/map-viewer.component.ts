f8f37535363fb8184eaa2fb608a136f4457e6f07
hdball04 <hannan.dissou@polymtl.ca>
2023-04-05 19:19:49 -0700

    found a solution to display arrow, testing with random values

--- Interface/client/src/app/components/map-viewer/map-viewer.component.ts
+++ Interface/client/src/app/components/map-viewer/map-viewer.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 const ROS3D = require('ros3d');
-import ROSLIB, { Ros, TFClient, Transform } from 'roslib';
+import ROSLIB, { Ros, TFClient } from 'roslib';
 import { IpHandlerService } from 'src/app/services/ip-handler.service';
 import { GridClient, MapViewer, UrdfClient } from 'src/app/types/ros3d';
 import { environment } from 'src/environments/environment';
@@ -22,11 +22,42 @@ export class MapViewerComponent {
 
     private gridClientMerged: GridClient = { };
 
-    private viewer1: MapViewer = { 'scene': undefined };
 
-    private viewer2: MapViewer = { 'scene': undefined };
+    private viewer1: MapViewer = { 
+        'scene':  { 
+            add: (arrowNode: any) => {
+              // implementation of add method
+            }, 
+            remove: (arrowNode: any) => {
+              // implementation of add method
+            } 
+          } 
+      };
+      
 
-    private viewerMerged: MapViewer = { 'scene': undefined };
+      private viewer2: MapViewer = { 
+        'scene':  { 
+          add: (arrowNode: any) => {
+            // implementation of add method
+          }, 
+          remove: (arrowNode: any) => {
+            // implementation of add method
+          } 
+        } 
+      };
+      
+
+      private viewerMerged: MapViewer = { 
+        'scene':  { 
+            add: (arrowNode: any) => {
+              // implementation of add method
+            }, 
+            remove: (arrowNode: any) => {
+              // implementation of add method
+            } 
+          } 
+      };
+      
 
     private ros1: Ros;
 
@@ -42,16 +73,13 @@ export class MapViewerComponent {
             'url': `ws://${ipHandler.ipAddressLimo2}:9090`
         });
 
-        /*
-         * Setup a client to listen to TFs.
-         * A TF Client that listens to TFs from tf2_web_republisher
-         */
+        // Create a TF client that subscribes to the fixed frame.
         this.tfClient = new ROSLIB.TFClient({
-            'ros': this.ros1,
-            'fixedFrame': 'world',
-            'angularThres': 0.01,
-            'transThres': 0.01,
-            'rate': 10.0
+            ros          : this.ros1,
+            angularThres : 0.1,
+            transThres   : 0.1,
+            rate         : 1.0,
+            fixedFrame   : '/map'
         });
 
 
@@ -112,22 +140,62 @@ export class MapViewerComponent {
         });
 
 
-        /*
-         * Setup the URDF client.
-         * create ROS3D.UrdfClient
-         * path: provide the source of urdf，package://localPath or meshed，: package:///body/meshed/body.dae
-         * or using http://, 如 http://localhost:8000/body/meshed/body.dae
-         * the loader will use robot_description as URDF model
-         * loader can use to kinds of Collada Loader， PR2 robot ussed ColladalLoader2, other robot model ColladaLoader THREE.js(ROS3D.COLLADA_LOADER)
-         */
-        this.urdfClient = new ROS3D.UrdfClient({
-            'ros': this.ros1,
-            'tfClient': this.tfClient,
-            'path': 'iiwa_stack',
-            'rootObject': this.viewer1.scene,
-            'loader': ROS3D.COLLADA_LOADER
-        });
+        /*var odomListener = new ROSLIB.Topic({
+            ros: this.ros1,
+            name: '/limo1/odom',
+            messageType: 'nav_msgs/Odometry'
+          });
+          
+          odomListener.subscribe((message: any) => {
+        // Extract the x, y, and z values from the `pose` field of the `Odometry` message
+        var x = message.pose.pose.position.x;
+        var y = message.pose.pose.position.y;
+        var z = message.pose.pose.position.z;
+        
+        // Update the arrow's position
+        arrow.origin.set(x, y, z);
+        
+        // Redraw the arrow on the viewer
+        this.viewer1.scene.remove(arrow); // remove the arrow from the scene
+        this.viewer1.scene.add(arrow); // add the arrow back to the scene at the new position
+        });*/
+
+        setInterval(()=> {
+            this.addArrow();
+        }, 3000);
+
     }
 
+    addArrow() {
+        console.log("adding arrow func...")
+        // Generate random x, y, and z values between 0 and 5
+        var xx = Math.random() * 5;
+        var yy = Math.random() * 5;
+        var zz = Math.random() * 5;
+      
+        // Create a new Arrow object with random parameters
+        var arrow = new ROS3D.Arrow({
+            ros: this.ros1,
+            length : 1.0,
+            shaftRadius : 0.05,
+            headRadius : 0.1,
+            headLength : 0.2,
+            color : "#ff0000",
+            x: xx,
+            y: yy,
+            z: zz 
+        });
+      
+        // Add the arrow to the viewer
+        this.viewer1.scene.add(arrow);
+        console.log("added!");
+        //Remove the arrow after 1.5 seconds
+        setTimeout(() => {
+        console.log("removing arrow...")
+          this.viewer1.scene.remove(arrow);
+          console.log("removed!");
+        }, 1500);
+      }
+      
 
 }
--- Interface/client/src/app/types/ros3d.ts
+++ Interface/client/src/app/types/ros3d.ts
@@ -1,5 +1,9 @@
-export type MapViewer = { scene: unknown}
+export type MapViewer = { scene: Scene}
 export type GridClient = { }
 export type SceneNode = { }
 export type UrdfClient = { }
+export type Scene = {
+    add(arrowNode: any): unknown
+    remove(arrowNode: any): unknown
+}
 
