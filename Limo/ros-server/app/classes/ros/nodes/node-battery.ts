20424968fc8a66a8bdd9697e4b4f860ef2bbd2ec
youssef137ch <youssef.chourifi@polymtl.ca>
2023-04-11 16:20:06 -0700

    removing unexpected keyword from node-battery file

--- Limo/ros-server/app/classes/ros/nodes/node-battery.ts
+++ Limo/ros-server/app/classes/ros/nodes/node-battery.ts
@@ -22,7 +22,6 @@ export class NodeBattery {
     // Wait for ROS to connect to the bridge
     this.ros.on('connection', () => {
       console.log(`${this.name} : ROS connected`);
-      Initialize subscriber
       this.batterySubscriber = new Topic({
         ros: this.ros,
         name: '/limo_status',
