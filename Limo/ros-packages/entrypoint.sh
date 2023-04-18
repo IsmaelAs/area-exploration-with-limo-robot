6247fc2d23cac201209c4de51079f20ee0192f3d
Koceila Lakhdari <koceila.lakhdari@polymtl.ca>
2023-04-18 04:54:05 -0700

    run restart node as last run

--- Limo/ros-packages/entrypoint.sh
+++ Limo/ros-packages/entrypoint.sh
@@ -38,11 +38,13 @@ if [ ! "$IS_SIMULATION" ];  then
   sleep 10
 
   # Launch explore lite
-  exec roslaunch --wait  limo_bringup one_exploration.launch  2> >(grep -v TF_REPEATED_DATA buffer_core)
+  roslaunch --wait  limo_bringup one_exploration.launch  2> >(grep -v TF_REPEATED_DATA buffer_core) &
+
+  rosrun update-pkg restart-package-container.py
 
 else 
-  exec rosrun explore_control control_explore.py
+  rosrun explore_control control_explore.py &
+  rosrun update-pkg restart-package-container.py
 
 fi
 
-# Subscribe to /exploration_state topic to control exploration state
