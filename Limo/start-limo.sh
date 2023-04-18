0f43eefd35bce754589097b7074ae2ccb8188b0a
Koceila Lakhdari <koceila.lakhdari@polymtl.ca>
2023-04-18 04:55:41 -0700

    change rm to restart for restraring containers

--- Limo/start-limo.sh
+++ Limo/start-limo.sh
@@ -24,8 +24,8 @@ if [ "$IS_SIMUALTION" == "true" ] || [ "$IS_SIMULATION" == "1" ]; then
 
   ./ros-packages/simulation/random-world-generator.sh
 
-  docker run --name ros-packages-server-1  --network host --rm -e ROS_MASTER_URI=$ROS_MASTER_URI -e IS_SIMULATION=1 -e LIMO_ID='1'  -d ros-packages-server
-  docker run --name ros-packages-server-2  --network host --rm -e ROS_MASTER_URI=$ROS_MASTER_URI -e IS_SIMULATION=1 -e LIMO_ID='2'  -d ros-packages-server
+  docker run --name ros-packages-server-1  --network host --restart always -e ROS_MASTER_URI=$ROS_MASTER_URI -e IS_SIMULATION=1 -e LIMO_ID='1'  -d ros-packages-server
+  docker run --name ros-packages-server-2  --network host --restart always -e ROS_MASTER_URI=$ROS_MASTER_URI -e IS_SIMULATION=1 -e LIMO_ID='2'  -d ros-packages-server
 
   sleep 10
 
@@ -34,18 +34,18 @@ if [ "$IS_SIMUALTION" == "true" ] || [ "$IS_SIMULATION" == "1" ]; then
   LIMO_IP_SIMU_2=$(docker logs ros-packages-server-2 | head -n1 | awk '{print $1;}')
   echo $LIMO_IP_SIMU_2
 
-  docker run --name ros-server-1 -p 9332:9332 --rm -e LIMO_IP=$LIMO_IP_SIMU_1 -e IS_SIMULATION=1 -e LIMO_ID='1'  -d ros-server
-  docker run --name ros-server-2 -p 9333:9333 --rm -e LIMO_IP=$LIMO_IP_SIMU_2 -e IS_SIMULATION=1 -e LIMO_ID='2'   ros-server
+  docker run --name ros-server-1 -p 9332:9332 --restart always -e LIMO_IP=$LIMO_IP_SIMU_1 -e IS_SIMULATION=1 -e LIMO_ID='1'  -d ros-server
+  docker run --name ros-server-2 -p 9333:9333 --restart always -e LIMO_IP=$LIMO_IP_SIMU_2 -e IS_SIMULATION=1 -e LIMO_ID='2'   ros-server
 
 
 else 
 
-  docker run --name ros-packages-server  --network host --rm -e ROS_MASTER_URI=$ROS_MASTER_URI -d ros-packages-server
+  docker run --name ros-packages-server  --network host --restart always -e ROS_MASTER_URI=$ROS_MASTER_URI -d ros-packages-server
   sleep 10
 
   LIMO_IP=$(docker logs ros-packages-server | head -n1 | awk '{print $1;}')
   echo $LIMO_IP
 
-  docker run --name ros-server -p 9332:9332 --rm -e LIMO_IP=$LIMO_IP   ros-server
+  docker run --name ros-server -p 9332:9332 --restart always -e LIMO_IP=$LIMO_IP   ros-server
 
 fi 
