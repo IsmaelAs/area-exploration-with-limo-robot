323e27b973dfae630285e929ef46773ef2e277e3
madysemega <madysemega.ms@gmail.com>
2023-04-20 06:34:10 +0000

    Update 4 files
    - /Limo/ros-packages/packages/src/explore_control/CMakeLists.txt
    - /Limo/ros-packages/packages/src/explore_control/package.xml
    - /Limo/ros-packages/packages/src/explore_control/src/return_to_base.py
    - /Limo/ros-packages/Dockerfile

--- Limo/ros-packages/packages/src/explore_control/src/return_to_base.py
+++ Limo/ros-packages/packages/src/explore_control/src/return_to_base.py
@@ -1,7 +1,8 @@
-# !/usr/bin/env python
+#!/usr/bin/env python
 
 import rospy
 from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Odometry  # Add this import
 from std_msgs.msg import Bool
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib
@@ -17,7 +18,7 @@ class ReturnToBase:
             self.limoId = os.environ.get("LIMO_ID", "1")
             rospy.init_node('return_to_base_node' + self.limoId)
 
-            self.pose_subscriber = rospy.Subscriber(f'/limo{self.limoId}/odom', PoseStamped, self.pose_callback)
+            self.pose_subscriber = rospy.Subscriber(f'/limo{self.limoId}/odom', Odometry, self.pose_callback)  # Update the message type
             self.return_subscriber = rospy.Subscriber(f'/limo{self.limoId}/return_to_base', Bool, self.return_callback)
 
             self.move_base_client = actionlib.SimpleActionClient(f'/limo{self.limoId}/move_base', MoveBaseAction)
@@ -25,7 +26,7 @@ class ReturnToBase:
         else:
             rospy.init_node('return_to_base_node')
 
-            self.pose_subscriber = rospy.Subscriber('/odom', PoseStamped, self.pose_callback)
+            self.pose_subscriber = rospy.Subscriber('/odom', Odometry, self.pose_callback)  # Update the message type
             self.return_subscriber = rospy.Subscriber('/return_to_base', Bool, self.return_callback)
 
             self.move_base_client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
@@ -33,7 +34,7 @@ class ReturnToBase:
 
     def pose_callback(self, msg):
         if self.base_pose is None:
-            self.base_pose = msg.pose
+            self.base_pose = msg.pose.pose  # Update this line to get the pose from the Odometry message
 
     def return_callback(self, msg):
         if msg.data and self.base_pose is not None:
@@ -54,45 +55,3 @@ class ReturnToBase:
 if __name__ == '__main__':
     return_to_base = ReturnToBase()
     rospy.spin()
-
-# #!/usr/bin/env python
-
-# import rospy
-# from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
-# from std_msgs.msg import Bool
-# from tf.transformations import quaternion_from_euler
-
-# class ReturnToBase:
-#     def __init__(self):
-        
-#         self.base_pose = None
-#         self.isSimulation = os.environ.get("IS_SIMULATION")
-#         if self.isSimulation : 
-#             self.limoId = os.environ.get("LIMO_ID", "1")
-#             rospy.init_node('return_to_base_node' + self.limoId)
-#             self.pose_subscriber = rospy.Subscriber(f'{self.limoId}/odom', PoseStamped, self.pose_callback)
-#             self.return_subscriber = rospy.Subscriber(f'{self.limoId}/return_to_base', Bool, self.return_callback)
-#             self.goal_publisher = rospy.Publisher(f'{self.limoId}/move_base_simple/goal', PoseStamped, queue_size=1)
-#         else:
-#             rospy.init_node('return_to_base_node')
-
-#             self.pose_subscriber = rospy.Subscriber('/odom', PoseStamped, self.pose_callback)
-#             self.return_subscriber = rospy.Subscriber('/return_to_base', Bool, self.return_callback)
-#             self.goal_publisher = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=1)
-
-#     def pose_callback(self, msg):
-#         if self.base_pose is None:
-#             self.base_pose = msg.pose
-
-#     def return_callback(self, msg):
-#         if msg.data and self.base_pose is not None:
-#             goal = PoseStamped()
-#             goal.header.frame_id = "map"
-#             goal.header.stamp = rospy.Time.now()
-#             goal.pose = self.base_pose
-
-#             self.goal_publisher.publish(goal)
-
-# if __name__ == '__main__':
-#     return_to_base = ReturnToBase()
-#     rospy.spin()
