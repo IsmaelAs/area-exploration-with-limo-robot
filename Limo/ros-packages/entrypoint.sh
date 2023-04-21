50a1e77939685eee51d2be8142bf0eeeff346831
madysemega <madysemega.ms@gmail.com>
2023-04-21 03:58:10 +0000

    Update file entrypoint.sh

--- Limo/ros-packages/entrypoint.sh
+++ Limo/ros-packages/entrypoint.sh
@@ -2,45 +2,42 @@
 
 source /opt/ros/noetic/setup.bash
 
-
 echo $(hostname -I)
 sleep 5
-
 
 source /agx_ws/devel/setup.bash --extend
 
 if [ ! "$IS_SIMULATION" ];  then 
-  cp -r ./packages/launchs $(rospack find limo_bringup)
-  cp -r ./packages/params $(rospack find limo_bringup)
+  cp -r ./packages/launchs $(rospack find limo_bringup) &&
+  cp -r ./packages/params $(rospack find limo_bringup) &&
 
-  roslaunch rosbridge_server rosbridge_websocket.launch &
-  wait
+  roslaunch rosbridge_server rosbridge_websocket.launch &&
+  wait &&
   # Launch gmapping
-  roslaunch --wait  limo_bringup one_gmapping.launch  2> >(grep -v TF_REPEATED_DATA buffer_core) &
+  roslaunch --wait  limo_bringup one_gmapping.launch  2> >(grep -v TF_REPEATED_DATA buffer_core) &&
 
   # Wait for gmapping to start up
-  sleep 5
+  sleep 5 &&
 
   # Launch map_merge
-  roslaunch --wait  limo_bringup map_merge.launch  2> >(grep -v TF_REPEATED_DATA buffer_core) &
+  roslaunch --wait  limo_bringup map_merge.launch  2> >(grep -v TF_REPEATED_DATA buffer_core) &&
 
   # Wait for map_merge to start up
-  sleep 5
+  sleep 5 &&
 
   # Launch navigation stack
-  roslaunch  --wait  limo_bringup one_navigation.launch  2> >(grep -v TF_REPEATED_DATA buffer_core) &
+  roslaunch  --wait  limo_bringup one_navigation.launch  2> >(grep -v TF_REPEATED_DATA buffer_core) &&
 
   # Wait for navigation stack to start up
-  sleep 5
+  sleep 5 &&
 
   # Subscribe to /exploration_state topic to control exploration state
-  echo "Launching explore_control..."
-  rosrun explore_control control_explore.py  &
-  rosrun explore_control return_to_base.py &
+  echo "Launching explore_control..." &&
+  rosrun explore_control control_explore.py  &&
+  rosrun explore_control return_to_base.py &&
   exec rosrun update-pkg restart-package-container.py
 else 
-  echo "Launching explore_control..."
-  rosrun explore_control control_explore.py &
+  echo "Launching explore_control..." &&
+  rosrun explore_control control_explore.py &&
   exec rosrun update-pkg restart-package-container.py
 fi
-wait
