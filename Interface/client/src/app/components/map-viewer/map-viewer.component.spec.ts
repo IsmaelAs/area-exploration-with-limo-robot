a67dffd6b2b73d8be1331360ec9670d605a6eca9
IsmaelAs <adamsoulerezaould@gmail.com>
2023-04-21 15:25:15 -0700

    Revert "Revert "Fix test crash""
    This reverts commit 24894636a4dce7e34a72a6918c64f74812c87482.

--- Interface/client/src/app/components/map-viewer/map-viewer.component.spec.ts
+++ Interface/client/src/app/components/map-viewer/map-viewer.component.spec.ts
@@ -61,13 +61,9 @@ describe('MapViewerComponent', () => {
 
     component.init();
 
-    expect(ROS3D.Viewer.prototype.constructor).toHaveBeenCalledTimes(3);
-    expect(ROS3D.OccupancyGridClient.prototype.constructor).toHaveBeenCalledTimes(3);
+    expect(ROS3D.Viewer.prototype.constructor).toHaveBeenCalled();
+    expect(ROS3D.OccupancyGridClient.prototype.constructor).toHaveBeenCalled();
 
-    expect(component["viewer1"]).toBeDefined();
-    expect(component["gridClient1"]).toBeDefined();
-    expect(component["viewer2"]).toBeDefined();
-    expect(component["gridClient2"]).toBeDefined();
     expect(component["viewerMerged"]).toBeDefined();
     expect(component["gridClientMerged"]).toBeDefined();
   });
@@ -79,65 +75,33 @@ describe('MapViewerComponent', () => {
     component.init();
 
     expect(component["ros1"]).toBeDefined();
-    expect(component["ros2"]).toBeDefined();
 
     expect(component["ros1"].on).toHaveBeenCalledWith('close', jasmine.any(Function));
-    expect(component["ros2"].on).toHaveBeenCalledWith('close', jasmine.any(Function));
 
     expect(component["ros1"].constructor).toHaveBeenCalledWith({
       'url': `ws://${ipAddressLimo1}:9090`
     });
-    expect(component["ros2"].constructor).toHaveBeenCalledWith({
-      'url': `ws://${ipAddressLimo2}:9090`
-    });
+ 
   });
 
   it('should set the topic correctly based on the environment', () => {
     spyOn(ROS3D.OccupancyGridClient.prototype, 'constructor');
 
-    const isSimulation = "true";
-    environment.IS_SIMULATION = isSimulation;
+    /*const isSimulation = true;
+    environment.IS_SIMULATION = isSimulation;*/
 
     component.init();
 
     expect(ROS3D.OccupancyGridClient.prototype.constructor).toHaveBeenCalledWith({
       'ros': component["ros1"],
-      'topic': '/limo1/map',
-      'rootObject': component["viewer1"].scene,
-      'continuous': true
-    });
-    expect(ROS3D.OccupancyGridClient.prototype.constructor).toHaveBeenCalledWith({
-      'ros': component["ros2"],
-      'topic': '/limo2/map',
-      'rootObject': component["viewer2"].scene,
-      'continuous': true
-    });
-    expect(ROS3D.OccupancyGridClient.prototype.constructor).toHaveBeenCalledWith({
-      'ros': component["ros1"],
-      'topic': '/map_merge/x_merged_map',
+      'topic': '/map_merged/map',
       'rootObject': component["viewerMerged"].scene,
       'continuous': true
     });
   });
-
-  it('should create the main viewer 1', () => {
-    expect(component["viewer1"]).toBeDefined();
-    });
-    
-    it('should create the main viewer 2', () => {
-    expect(component["viewer2"]).toBeDefined();
-    });
     
     it('should create the merged viewer', () => {
     expect(component["viewerMerged"]).toBeDefined();
-    });
-    
-    it('should create the grid client 1', () => {
-    expect(component["gridClient1"]).toBeDefined();
-    });
-    
-    it('should create the grid client 2', () => {
-    expect(component["gridClient2"]).toBeDefined();
     });
     
     it('should create the merged grid client', () => {
@@ -148,57 +112,12 @@ describe('MapViewerComponent', () => {
     expect(component["ros1"]).toBeDefined();
     });
     
-    it('should create the ros 2', () => {
-    expect(component["ros2"]).toBeDefined();
-    });
-    
     describe('init', () => {
     beforeEach(() => {
     spyOn(ROS3D, 'Viewer').and.callThrough();
     spyOn(ROS3D, 'OccupancyGridClient').and.callThrough();
     component.init();
     });
-  });
-  it('should create the main viewer 1', () => {
-    expect(ROS3D.Viewer).toHaveBeenCalledWith({
-      'background': '#7e7e7e',
-      'divID': 'map1',
-      'width': 800,
-      'height': 600,
-      'antialias': true
-    });
-    expect(component["viewer1"]).toBeDefined();
-  });
-  
-  it('should setup the grid client 1', () => {
-    expect(ROS3D.OccupancyGridClient).toHaveBeenCalledWith({
-      'ros': component["ros1"],
-      'topic': environment.IS_SIMULATION ? '/limo1/map' : '/map',
-      'rootObject': component["viewer1"].scene,
-      'continuous': true
-    });
-    expect(component["gridClient1"]).toBeDefined();
-  });
-  
-  it('should create the main viewer 2', () => {
-    expect(ROS3D.Viewer).toHaveBeenCalledWith({
-      'background': '#7e7e7e',
-      'divID': 'map2',
-      'width': 800,
-      'height': 600,
-      'antialias': true
-    });
-    expect(component["viewer2"]).toBeDefined();
-  });
-  
-  it('should setup the grid client 2', () => {
-    expect(ROS3D.OccupancyGridClient).toHaveBeenCalledWith({
-      'ros': component["ros2"],
-      'topic': environment.IS_SIMULATION ? '/limo2/map' : 'map',
-      'rootObject': component["viewer2"].scene,
-      'continuous': true
-    });
-    expect(component["gridClient2"]).toBeDefined();
   });
   
   it('should create the merged viewer', () => {
--- Interface/client/src/app/components/robots-actions/robots-actions.component.spec.ts
+++ Interface/client/src/app/components/robots-actions/robots-actions.component.spec.ts
@@ -24,11 +24,4 @@ describe('RobotsActionsComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it("should set robotTarget when we call setTarget", () => {
-    component.robotTarget = "limo-1"
-
-    component.setTarget("robots")
-
-    expect(component.robotTarget).toBe("robots")
-  })
 });
