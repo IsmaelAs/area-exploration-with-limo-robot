e051202d7243e227aff578989537ea37b35e7fd4
madysemega <madysemega.ms@gmail.com>
2023-04-22 00:30:22 +0000

    Update file map-viewer.component.ts

--- Interface/client/src/app/components/map-viewer/map-viewer.component.ts
+++ Interface/client/src/app/components/map-viewer/map-viewer.component.ts
@@ -51,7 +51,7 @@ export class MapViewerComponent {
         // Setup the Merged map client.
         this.gridClientMerged = new ROS3D.OccupancyGridClient({
             'ros': this.ros1,
-            'topic': environment.IS_SIMULATION ? '/map_merge/x_merged_map' : '/map_merged/map',
+            'topic': '/map_merge/x_merged_map',
             'rootObject': this.viewerMerged.scene,
             'continuous': true
         });
